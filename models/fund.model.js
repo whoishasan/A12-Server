6bb078e6d11f173bad2c2b4b9f5cb3814e01fe2a
whoishasan <uniquest.hasan@gmail.com>
2025-01-29 19:56:15 +0600

    fund model

--- models/fund.model.js
+++ models/fund.model.js
@@ -2,23 +2,26 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const FundSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const FundSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    note: {
+      type: String,
+    },
+    ammount: {
+      type: Number,
+      required: true,
+    },
   },
-  avatar: {
-    type: String,
-    required: true,
-  },
-  note: {
-    type: String,
-  },
-  ammount: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const FundModel = model("Funds", FundSchema);
 
