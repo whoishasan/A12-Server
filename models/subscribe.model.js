c25dcd0cca5961d40786c74dc7b558ebbcc0eabf
whoishasan <uniquest.hasan@gmail.com>
2025-01-29 19:57:13 +0600

    subscribe model

--- models/subscribe.model.js
+++ models/subscribe.model.js
@@ -2,13 +2,16 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const subscribeSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const subscribeSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Subscribe = model("Subscribe", subscribeSchema);
 
